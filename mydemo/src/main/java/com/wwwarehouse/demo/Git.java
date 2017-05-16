4190a89b1616aecf1384b911b77e65f51249f852
chaoyong.qin <chaoyong.qin@wwwarehouse.com>
2017-05-16 10:58:23 +0800

    添加mydemo工程

---
+++ mydemo/src/main/java/com/wwwarehouse/demo/Git.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.wwwarehouse.demo;
+
+/**
+ * @author chaoyong.qin
+ *
+ */
+public class Git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (String string : args) {
+			System.out.println(string);
+		}
+		
+		System.out.println("Git.main()");
+		
+		// 正常退出
+		System.exit(0);
+
+	}
+
+}
