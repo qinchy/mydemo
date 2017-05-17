8eaa59ac2c1f78a0e6073167d8213f938f4cbb50
chaoyong.qin <chaoyong.qin@wwwarehouse.com>
2017-05-17 16:16:38 +0800

    添加mydemo工程

---
+++ mydemo/src/main/java/com/wwwarehouse/demo/CompletionServiceTest.java
@@ -0,0 +1,27 @@
+package com.wwwarehouse.demo;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by chaoyong.qin on 2017/5/17.
+ */
+public class CompletionServiceTest {
+    public static void main(String[] args) throws Exception{
+        ExecutorService exec = Executors.newCachedThreadPool();
+        CompletionService<String> cs = new ExecutorCompletionService<String>(exec);
+        for (int i=0;i<10;i++){
+            final int j = i;
+            cs.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("第"+j+"个使用的线程是" +Thread.currentThread().getName());
+                }
+            },String.valueOf(i));
+        }
+
+        for (int i=0;i<10;i++){
+            Future<String> take = cs.take();
+            System.out.println(take.get());
+        }
+    }
+}
